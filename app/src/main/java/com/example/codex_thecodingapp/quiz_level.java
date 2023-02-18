d8ebcfc1ce095d8c8177bdcba3554b99c22e3423
Shubham5250 <shubhampaw312@gmail.com>
2023-02-18 23:46:50 +0530

    initialized changes

---
+++ app/src/main/java/com/example/codex_thecodingapp/quiz_level.java
@@ -0,0 +1,14 @@
+package com.example.codex_thecodingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class quiz_level extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_level);
+    }
+}
