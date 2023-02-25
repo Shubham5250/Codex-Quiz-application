cd0702e04ab5e22e57b0673c2ee8759583072c4f
Shubham5250 <shubhampaw312@gmail.com>
2023-02-26 02:23:04 +0530

    SplashScreen Activity added

--- app/src/main/java/com/example/codex_thecodingapp/SplashScreen.java
+++ app/src/main/java/com/example/codex_thecodingapp/SplashScreen.java
@@ -8,7 +8,7 @@ import android.os.Bundle;
 import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 900;
+    private static int SPLASH_TIME_OUT = 1900;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
